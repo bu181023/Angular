9a4afde9492f869c5e27205146bfe3bc1c81b8c3
VIGNESH K <bu181023@gmail.com>
2021-06-23 17:01:11 +0000

    Complete

--- petStore/src/app/pet-store/user-details/user-details.component.ts
+++ petStore/src/app/pet-store/user-details/user-details.component.ts
@@ -6,26 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-details.component.css'],
 })
 export class UserDetailsComponent implements OnInit {
-  panelOpenState :boolean = false;
+  panelOpenState: boolean = false;
   data: any[] = [
     {
-      swagger: '2.0',
-      info: {
-        version: '1.0.0',
-        title: 'Swagger Petstore',
-        description:
-          'A sample API that uses a petstore as an example to demonstrate features in the swagger-2.0 specification',
-        termsOfService: 'http://swagger.io/terms/',
-        contact: {
-          name: 'Swagger API Team',
-          email: 'apiteam@swagger.io',
-          url: 'http://swagger.io',
+      name: 'vignesh',
+      info: [
+        {
+          version: '1.0.0',
+          title: 'Swagger Petstore',
+          description:
+            'A sample API that uses a petstore as an example to demonstrate features in the swagger-2.0 specification',
+          termsOfService: 'http://swagger.io/terms/',
+          contact: [
+            {
+              name: 'Swagger API Team',
+              email: 'apiteam@swagger.io',
+              url: 'http://swagger.io',
+            },
+          ],
         },
-        license: {
+      ],
+      license: [
+        {
           name: 'Apache 2.0',
           url: 'https://www.apache.org/licenses/LICENSE-2.0.html',
         },
-      },
+      ],
     },
   ];
   constructor() {}
